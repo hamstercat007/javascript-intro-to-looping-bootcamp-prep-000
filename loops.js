0eb178d2ff7ffc5af5e90facfab76763980872e2
Lisa Mac <lisamac421@gmail.com>
2018-05-08 18:14:57 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
-
-
-for (let i = 0; i < 25; i++) {
-  console.log(`"I am ${i} strange loop."`)
+function forLoop() {
+  for (let i = 0; i < 25; i++) {
+    console.log(`"I am ${i} strange loop."`)  
+  }
 }
